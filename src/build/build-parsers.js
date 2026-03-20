f17bba02f257ed6cfcb2f771759b156b1a11a02e
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-03-20 02:12:28 -0400

    Update build-parsers.js

--- src/build/build-parsers.js
+++ src/build/build-parsers.js
@@ -20,6 +20,7 @@ import path from 'node:path';
 import os from 'node:os';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
+ import {BUILTIN_GRAMMARS} from './grammars.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
